d6ccc54ff85e65f582e0a7cce0b56085b29213b2
bala-amypo <bala@amypo.in>
2025-12-22 08:26:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/Location.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
