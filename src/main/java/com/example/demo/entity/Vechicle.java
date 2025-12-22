f9fb03cee9302200457d088848de6f22780109ba
bala-amypo <bala@amypo.in>
2025-12-22 07:50:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Vechicle.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @Column(unique = true)
+    private String vehicleNumber;
+    
+    private Double capacityKg;
+    
+    private Double fuelEfficiency;
+    
+    @OneToMany(mappedBy = "vehicle")
+    private List<Shipment> shipments;
+    
+    public Vehicle() {}
+    
+    public Vehicle(User user, String vehicleNumber, Double capacityKg, Double fuelEfficiency) {
+        this.user = user;
+        this.vehicleNumber = vehicleNumber;
+        this.capacityKg = capacityKg;
+        this.fuelEfficiency = fuelEfficiency;
+    }
+    
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    
+    public String getVehicleNumber() { return vehicleNumber; }
+    public void setVehicleNumber(String vehicleNumber) { this.vehicleNumber = vehicleNumber; }
+    
+    public Double getCapacityKg() { return capacityKg; }
+    public void setCapacityKg(Double capacityKg) { this.capacityKg = capacityKg; }
+    
+    public Double getFuelEfficiency() { return fuelEfficiency; }
+    public void setFuelEfficiency(Double fuelEfficiency) { this.fuelEfficiency = fuelEfficiency; }
+    
+    public List<Shipment> getShipments() { return shipments; }
+    public void setShipments(List<Shipment> shipments) { this.shipments = shipments; }
+}
