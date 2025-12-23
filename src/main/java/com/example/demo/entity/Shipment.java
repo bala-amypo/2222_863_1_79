9de6746965196cf23b0d5fac6f77ad4c16ec0b73
bala-amypo <bala@amypo.in>
2025-12-23 04:23:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Shipment.java
+++ src/main/java/com/example/demo/entity/Shipment.java
@@ -4,53 +4,23 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "shipments")
 public class Shipment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @ManyToOne
-    @JoinColumn(name = "vehicle_id")
     private Vehicle vehicle;
-    
+
     @ManyToOne
-    @JoinColumn(name = "pickup_location_id")
     private Location pickupLocation;
-    
+
     @ManyToOne
-    @JoinColumn(name = "drop_location_id")
     private Location dropLocation;
-    
+
     private Double weightKg;
-    
     private LocalDate scheduledDate;
-    
-    public Shipment() {}
-    
-    public Shipment(Vehicle vehicle, Location pickupLocation, Location dropLocation, Double weightKg, LocalDate scheduledDate) {
-        this.vehicle = vehicle;
-        this.pickupLocation = pickupLocation;
-        this.dropLocation = dropLocation;
-        this.weightKg = weightKg;
-        this.scheduledDate = scheduledDate;
-    }
-    
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public Vehicle getVehicle() { return vehicle; }
-    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
-    
-    public Location getPickupLocation() { return pickupLocation; }
-    public void setPickupLocation(Location pickupLocation) { this.pickupLocation = pickupLocation; }
-    
-    public Location getDropLocation() { return dropLocation; }
-    public void setDropLocation(Location dropLocation) { this.dropLocation = dropLocation; }
-    
-    public Double getWeightKg() { return weightKg; }
-    public void setWeightKg(Double weightKg) { this.weightKg = weightKg; }
-    
-    public LocalDate getScheduledDate() { return scheduledDate; }
-    public void setScheduledDate(LocalDate scheduledDate) { this.scheduledDate = scheduledDate; }
+
+    // getters and setters
 }
