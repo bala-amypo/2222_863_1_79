189920090c6d96cb5150b442446955fe3fc50de8
bala-amypo <bala@amypo.in>
2025-12-23 09:54:47 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,24 +1,4 @@
-package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth..requestMatchers(
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**"
-                
-                .anyRequest().permitAll()
-            );
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
