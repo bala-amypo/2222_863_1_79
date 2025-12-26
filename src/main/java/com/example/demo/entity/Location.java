ba08cbe94f4d0365083202d8a0d868c54e4d202c
bala-amypo <bala@amypo.in>
2025-12-26 12:22:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,9 +1,17 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "locations")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,24 +22,4 @@ public class Location {
     private Double latitude;
     
     private Double longitude;
-
-    public Location() {}
-
-    public Location(String name, Double latitude, Double longitude) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public Double getLatitude() { return latitude; }
-    public void setLatitude(Double latitude) { this.latitude = latitude; }
-    
-    public Double getLongitude() { return longitude; }
-    public void setLongitude(Double longitude) { this.longitude = longitude; }
 }
